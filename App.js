06cc7b94e64bb98d3f939cff77031f03cf65ff55
rstotler <rstotler87@gmail.com>
2023-09-03 11:02:04 -0400

    User creation

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigator from "./src/navigation";
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth, API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import awsconfig from './src/aws-exports';
+import { getUser } from './src/graphql/queries';
+import { createUser } from './src/graphql/mutations';
 
 Amplify.configure({...awsconfig, Analytics: { disabled: true }});
 
 function App() {
+  console.log("Initializing app..");
+
+  useEffect(() => {
+    const syncUser = async () => {
+      const authUser = await Auth.currentAuthenticatedUser({bypassCache: true});
+      const userData = await API.graphql(graphqlOperation(getUser, { id: authUser.attributes.sub }));
+      console.log("User: " + userData.data.getUser);
+      if(userData.data.getUser) {
+        console.log("User already exists in DB");
+        return;
+      }
+
+      const newUser = {
+        id: authUser.attributes.sub,
+        name: authUser.attributes.phone_number,
+        status: "Hey, I am using LetsChat!",
+      };
+      const newUserResponse = await API.graphql(graphqlOperation(createUser, { input: newUser }));
+    };
+
+    syncUser();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
